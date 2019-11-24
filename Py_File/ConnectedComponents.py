a2205f9e919b59548b0638c660e627476aa897fb
kishansinghverma <kishansinghverma@gmail.com>
2019-11-25 00:46:55 +0530

    direct

--- Py_File/ConnectedComponents.py
+++ Py_File/ConnectedComponents.py
@@ -1,3 +1,5 @@
+import queue
+
 def getGraph(edges):
     arr=[]
     for x in edges:
@@ -7,19 +9,58 @@ def getGraph(edges):
     arr=list(set(arr))
     nodes=len(arr)
     key=dict()
+    mapper=dict()
     arr.sort()
     for i, x in enumerate(arr):
         key[x]=i+1
+        mapper[i+1]=x
 
     graph=[list() for _ in range(nodes+1)]
-
     for x in edges:
         graph[key[x[0]]].append(key[x[1]])
 
-    return graph
+    return graph,  mapper
 
-#edges=[[0,4],[6,2],[2,0],[1,6],[4,2],[0,3],[3,5],[5,3],[5,7],[7,5],[7,6]]
-edges=[[18,17],[16,18],[16,25],[18,21],[25,17]]
-g=getGraph(edges)
+def getMappedSearch(bfs, mapper):
+    print("\nBFS Of Given Graph Is:\n(", end="")
+    for x in bfs[:-1]:
+        print(mapper[x], end=", ")
+    print(str(mapper[bfs[-1]])+")")
 
-print(g)
+
+def getMappedGraph(graph, mapper):
+    print("\nAdj. List View Of Given Graph Is:")
+    for i, x in enumerate(graph):
+        if(len(x)>0):
+            print(mapper[i], "->", end=" (")
+            for y in x[:-1]:
+                print(mapper[y], end=", ")
+            print(str(mapper[x[-1]])+")")
+
+def doBFS(graph, start):
+    bfs=[]
+    visited=[False]*len(graph)
+    q=queue.Queue()
+    bfs.append(start)
+    for x in graph[start]:
+        q.put(x)
+        bfs.append(x)
+
+    visited[start]=True
+    while(not q.empty()):
+        n=q.get()
+        for x in graph[n]:
+            if(not visited[x]):
+                q.put(x)
+                bfs.append(x)
+        visited[n]=True
+    return bfs
+
+edges=[[1,0],[1,3],[3,1],[0,3],[1,2],[0,4],[6,2],[5,6],[7,6],[6,4]]
+graph, mapper = getGraph(edges)
+
+getMappedGraph(graph, mapper)
+
+bfs=doBFS(graph, 1)
+
+getMappedSearch(bfs, mapper)
